909ef4ed9f6a6ab5b6878e14d66094ce03d53301
Stephen Yeargin <stephen.yeargin@gmail.com>
2020-01-07 23:21:42 -0600

    Alert view

--- public/assets/javascript/routes.js
+++ public/assets/javascript/routes.js
@@ -22,6 +22,7 @@ var routesLayer = L.layerGroup().addTo(map)
 
 var routeShapes = {}
 var routeData = {
+  route_gid: $('#route_header').data('route_gid'),
   route_color: $('#route_header').data('route_color'),
 }
 $('tbody tr').each(function(i, el) {
@@ -52,7 +53,65 @@ $('tbody tr').each(function(i, el) {
     }
   }
 })
+// Display Alerts
+var displayRouteAlerts = function () {
+  var alertTypeCounts = {}
+  $.get(GTFS_BASE_URL + '/realtime/alerts.json').done(function (alertData) {
+    $(alertData).each(function(i, message) {
+      $(message.alert.informed_entity).each(function(i, entity) {
+        if (entity.route_id == routeData.route_gid) {
+
+          if (!message.alert.effect) {
+            message.alert.effect = 'Notice'
+          }
+
+          var alertType = message.alert.effect.toLowerCase().replace(' ', '_')
+
+          var alert_class = 'info'
+          if (message.alert.effect == 'Detour' || message.alert.effect == 'Significant Delays') {
+            alert_class = 'warning'
+          }
+          if (message.alert.effect == 'Reduced Service' || message.alert.effect == 'No Service') {
+            alert_class = 'danger'
+          }
+
+          // Create the container for the alerts if not present
+          if (!$('#alert-group-' + alertType).length) {
+            alertTypeCounts[alertType] = 0
+            $('#alertGroup').append(L.Util.template(
+              $('#alert_group_item_template').html(),
+              {
+                type: alertType,
+                displayType: message.alert.effect
+              }
+            ))
+          }
+
+          var content = L.Util.template(
+            $('#alert_template').html(),
+            {
+              alert_class: alert_class,
+              alert_effect: message.alert.effect,
+              alert_cause: message.alert.cause ? ' (' + message.alert.cause +')' : '',
+              alert_heading: message.alert.header_text.translation[0].text,
+              alert_body: message.alert.description_text.translation[0].text.replace(/(\n)/g, '<br />'),
+              start_date: moment.unix(message.alert.active_period[0].start).format('l h:mm a'),
+              end_date: moment.unix(message.alert.active_period[0].end).format('l h:mm a')
+            }
+          )
+          $('#alert-group-' + alertType).append(content)
+          // Increment counter
+          alertTypeCounts[alertType]++
+          $('#alert-group-count-' + alertType).html(
+            alertTypeCounts[alertType]
+          )
+        }
+      })
+    })
+  })
+}
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+  displayRouteAlerts()
 })
