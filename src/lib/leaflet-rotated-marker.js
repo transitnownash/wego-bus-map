dcb9dc139115158e4b2f8a2d73802175a5b10060
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-07-13 23:11:27 -0500

    Fix ESLint warning

--- src/lib/leaflet-rotated-marker.js
+++ src/lib/leaflet-rotated-marker.js
@@ -7,84 +7,84 @@
  **/
 (function (L) {
   // save these original methods before they are overwritten
-  var protoInitIcon = L.Marker.prototype._initIcon
-  var protoSetPos = L.Marker.prototype._setPos
+  var protoInitIcon = L.Marker.prototype._initIcon;
+  var protoSetPos = L.Marker.prototype._setPos;
 
-  var oldIE = (L.DomUtil.TRANSFORM === 'msTransform')
+  var oldIE = (L.DomUtil.TRANSFORM === 'msTransform');
 
   L.Marker.addInitHook(function () {
-    var iconAnchor = this.options.icon.options.iconAnchor
-    var shadowAnchor = this.options.icon.options.shadowAnchor
+    var iconAnchor = this.options.icon.options.iconAnchor;
+    var shadowAnchor = this.options.icon.options.shadowAnchor;
     if (iconAnchor) {
-      iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px')
+      iconAnchor = (iconAnchor[0] + 'px ' + iconAnchor[1] + 'px');
     }
     if (shadowAnchor) {
-      shadowAnchor = (shadowAnchor[0] + 'px ' + shadowAnchor[1] + 'px')
+      shadowAnchor = (shadowAnchor[0] + 'px ' + shadowAnchor[1] + 'px');
     }
-    this.options.rotationShadowOrigin = this.options.rotationShadowOrigin || shadowAnchor || 'center bottom'
-    this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom'
-    this.options.rotationAngle = this.options.rotationAngle || 0
-    this.options.rotationShadowAngle = this.options.rotationShadowAngle || 0
-  })
+    this.options.rotationShadowOrigin = this.options.rotationShadowOrigin || shadowAnchor || 'center bottom';
+    this.options.rotationOrigin = this.options.rotationOrigin || iconAnchor || 'center bottom';
+    this.options.rotationAngle = this.options.rotationAngle || 0;
+    this.options.rotationShadowAngle = this.options.rotationShadowAngle || 0;
+  });
 
   L.Marker.include({
     _initIcon: function () {
-      protoInitIcon.call(this)
+      protoInitIcon.call(this);
     },
 
     _setPos: function (pos) {
-      protoSetPos.call(this, pos)
+      protoSetPos.call(this, pos);
 
       if (this.options.rotationAngle || this.options.rotationShadowOrigin) {
         if (this._icon) {
-          this._icon.style[L.DomUtil.TRANSFORM + 'Origin'] = this.options.rotationOrigin
+          this._icon.style[L.DomUtil.TRANSFORM + 'Origin'] = this.options.rotationOrigin;
         }
         if (this._shadow) {
-          this._shadow.style[L.DomUtil.TRANSFORM + 'Origin'] = this.options.rotationShadowOrigin
+          this._shadow.style[L.DomUtil.TRANSFORM + 'Origin'] = this.options.rotationShadowOrigin;
         }
 
         if (oldIE) {
           // for IE 9, use the 2D rotation
           if (this._icon) {
-            this._icon.style[L.DomUtil.TRANSFORM] = ' rotate(' + this.options.rotationAngle + 'deg)'
+            this._icon.style[L.DomUtil.TRANSFORM] = ' rotate(' + this.options.rotationAngle + 'deg)';
           }
           if (this._shadow) {
-            this._shadow.style[L.DomUtil.TRANSFORM] = ' rotate(' + this.options.rotationShadowAngle + 'deg)'
+            this._shadow.style[L.DomUtil.TRANSFORM] = ' rotate(' + this.options.rotationShadowAngle + 'deg)';
           }
         } else {
           // for modern browsers, prefer the 3D accelerated version
           if (this._icon) {
-            this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)'
+            this._icon.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationAngle + 'deg)';
           }
           if (this._shadow) {
-            this._shadow.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationShadowAngle + 'deg)'
+            this._shadow.style[L.DomUtil.TRANSFORM] += ' rotateZ(' + this.options.rotationShadowAngle + 'deg)';
           }
         }
       }
     },
 
     setRotationAngle: function (angle) {
-      this.options.rotationAngle = angle
-      this.update()
-      return this
+      this.options.rotationAngle = angle;
+      this.update();
+      return this;
     },
 
     setRotationShadowAngle: function (angle) {
-      this.options.rotationShadowAngle = angle
-      this.update()
-      return this
+      this.options.rotationShadowAngle = angle;
+      this.update();
+      return this;
     },
 
     setRotationOrigin: function (origin) {
-      this.options.rotationOrigin = origin
-      this.update()
-      return this
+      this.options.rotationOrigin = origin;
+      this.update();
+      return this;
     },
 
     setRotationShadowOrigin: function (origin) {
-      this.options.rotationShadowOrigin = origin
-      this.update()
-      return this
+      this.options.rotationShadowOrigin = origin;
+      this.update();
+      return this;
     }
-  })
-})(L)
+  });
+})(L);
