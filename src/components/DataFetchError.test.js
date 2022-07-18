5ca1dbeee1208608c190b13e29f1cffcbcac4058
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-07-17 21:50:06 -0500

    Fix DataFetchError test

--- src/components/DataFetchError.test.js
+++ src/components/DataFetchError.test.js
@@ -7,5 +7,6 @@ import { createRoot } from 'react-dom/client';
 test('renders DataFetchError', () => {
   const div = document.createElement('div');
   const root = createRoot(div);
-  root.render(<DataFetchError />);
+  const error = new Error('A runtime error has occurred.');
+  root.render(<DataFetchError error={error} />);
 });
