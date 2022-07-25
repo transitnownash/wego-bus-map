a7163cda8e6bb1220a7ab2301179ec9dbca0f4f5
Stephen Yeargin <stephen.yeargin@gmail.com>
2022-07-24 21:42:44 -0500

    Augment test coverage for TripTable

--- src/components/TripTable.test.js
+++ src/components/TripTable.test.js
@@ -1,11 +1,46 @@
-/* globals test */
+/* globals test, expect, afterEach */
 
 import React from 'react';
 import TripTable from './TripTable';
-import { createRoot } from 'react-dom/client';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+const routeTripsFixture = require('../fixtures/routes-4-trips.json');
+const routeFixture = require('../fixtures/routes-4.json');
+import MockDate from 'mockdate';
+
+afterEach(() => {
+  MockDate.reset();
+});
 
 test('renders TripTable', () => {
-  const div = document.createElement('div');
-  const root = createRoot(div);
-  root.render(<TripTable />);
+  MockDate.set('Fri Jul 22 2022 12:10:00 GMT-0500');
+  const {container} = render(
+    <TripTable routeTrips={routeTripsFixture.data} route={routeFixture} />
+  );
+  expect(screen.getAllByText('4 - DOWNTOWN')).toHaveLength(16);
+  expect(screen.getAllByText('4 - INGLEWOOD')).toHaveLength(16);
+  expect(screen.queryAllByText('266924')).toHaveLength(0);
+  expect(screen.queryAllByText('11:26 AM')).toHaveLength(0);
+  expect(screen.getByText('266927')).toBeInTheDocument();
+  expect(screen.getByText('12:09 PM')).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
+});
+
+test('renders TripTable with past trips', async () => {
+  MockDate.set('Sun Jul 24 2022 12:10:00 GMT-0500');
+  const user = userEvent.setup();
+
+  const {container} = render(
+    <TripTable routeTrips={routeTripsFixture.data} route={routeFixture} hidePastTrips={false} />
+  );
+
+  await user.click(screen.getByRole('checkbox', {name: /Hide Past Trips/i}));
+
+  expect(screen.getAllByText('4 - DOWNTOWN')).toHaveLength(26);
+  expect(screen.getAllByText('4 - INGLEWOOD')).toHaveLength(25);
+  expect(screen.getByText('266924')).toBeInTheDocument();
+  expect(screen.getByText('11:26 AM')).toBeInTheDocument();
+  expect(screen.getByText('266927')).toBeInTheDocument();
+  expect(screen.getByText('12:09 PM')).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
