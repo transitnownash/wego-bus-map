e3fd9f41b42b5d898ec77c1ea6d74c1c62ba0096
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-09-23 06:32:50 -0500

    Add tooltips

--- src/components/MapLinks.js
+++ src/components/MapLinks.js
@@ -4,12 +4,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import transitNowIcon from '../resources/transitnow.png';
 import './MapLinks.scss';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 
 function MapLinks() {
   return (
     <div className="map-links">
-      <Link to="/about" className="leaflet-control"><img src={transitNowIcon} height="35" width="35" alt="Transit Now" /></Link>
-      <Link to="/routes" className="leaflet-control"><FontAwesomeIcon icon={faRoute} fixedWidth={true} size={'2x'} /></Link>
+      <OverlayTrigger placement='left' overlay={<Tooltip>About</Tooltip>}>
+        <Link to="/about" className="leaflet-control"><img src={transitNowIcon} height="35" width="35" alt="Transit Now" /></Link>
+      </OverlayTrigger>
+      <OverlayTrigger placement='left' overlay={<Tooltip>Routes</Tooltip>}>
+        <Link to="/routes" className="leaflet-control"><FontAwesomeIcon icon={faRoute} fixedWidth={true} size={'2x'} /></Link>
+      </OverlayTrigger>
     </div>
   );
 }
