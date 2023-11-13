5cc06f99ee94e128621422ecc5ce27796a37819f
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-11-12 22:45:27 -0600

    Analytics

--- src/App.js
+++ src/App.js
@@ -14,6 +14,15 @@ import './resources/bootstrap-with-mixins.scss';
 import './App.scss';
 
 function App() {
+  React.useEffect(() => {
+    // BEGIN Matomo Analytics
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
+    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+    g.async = true; g.src = 'https://analytics.yearg.in/js/container_mRvU3guK.js'; s.parentNode.insertBefore(g, s);
+    // END Matomo Analytics
+  }, []);
+
   return (
     <ErrorBoundary>
       <BrowserRouter>
--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,9 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders App', () => {
+  // Fix for Matomo issue in tests
+  let emptyScriptTag = document.createElement('script');
+  document.getElementsByTagName('body')[0].appendChild(emptyScriptTag);
+
   render(<App />);
 });
